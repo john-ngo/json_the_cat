41c82b97af68cc06ad78f8c72bd7449b0a090c83
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-01-19 17:08:04 -0700

    solution

---
+++ breedFetcher.js
@@ -0,0 +1,15 @@
+const request = require("request");
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    
+    if (data.length === 0) {
+      console.log("Breed not found");
+    } else {
+      console.log(data[0].description);
+    }
+  }
+});
